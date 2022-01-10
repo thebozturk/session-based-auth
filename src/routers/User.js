57ca37139ab4a615cde7f997f281eb3969e807a4
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2022-01-10 15:32:41 +0300

    Add the new user router

--- src/routers/User.js
+++ src/routers/User.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const User = require("../controllers/User");
+const verifyToken = require("../middlewares/authenticate");
 
 const router = express.Router();
 const Controller = new User();
 
 router.post("/register", Controller.register);
-router.post("/signin", Controller.signin);
+router.post("/login", (req, res) => {
+  verifyToken, Controller.login(req, res);
+  res.cookie("access_token", req.token);
+});
+router.post("/logout", Controller.logout);
 
 module.exports = router;
