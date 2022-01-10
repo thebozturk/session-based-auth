0036a6a4f5fdc5c904892b9af22ccd55cf9b38a3
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2022-01-10 16:01:50 +0300

    Lınt code

--- src/routers/User.js
+++ src/routers/User.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const User = require("../controllers/User");
-const verifyToken = require("../middlewares/authenticate");
 
 const router = express.Router();
 const Controller = new User();
 
 router.post("/register", Controller.register);
 router.post("/login", (req, res) => {
-  verifyToken, Controller.login(req, res);
+  Controller.login(req, res);
   res.cookie("access_token", req.token);
 });
-router.post("/logout", Controller.logout);
+router.post("/logout", (req, res) => {
+  Controller.logout(req, res);
+});
 
 module.exports = router;
